c1a93a596a5ee3e568a4a9dd089e79143ffad4b2
imdn <md.imad@gmail.com>
2014-08-12 01:00:35 +0200

    Added mediainfo-cli, same A/V bitrate support

--- ffmpy.py
+++ ffmpy.py
@@ -3,13 +3,18 @@
 Created on Sun Aug 10 19:17:03 2014
 
 @author: imad
-Wrapper for ffmpeg cli. Simply takes a video file and encodes it to mp4 as per default settings. Parameters can be changed.
+- Wrapper for ffmpeg cli. Simply takes a video file and encodes it to mp4 as per default settings. Parameters can be changed.
+- Dependencies: ffmpeg (incl. x264, libaac/libfdk_aac support), mediainfo-cli
+
 """
 
 import os
-import sys, re
+import sys
 import argparse
 import subprocess
+from pymediainfo import MediaInfo
+import time
+from time import localtime, strftime
 
 def get_vargs():
     if args.vcopy:
@@ -17,9 +22,12 @@ def get_vargs():
         return v_args
     
     v_args = ['-c:v', args.vcodec]     
- 
     v_opts = ['-crf', args.crf]
-    if args.vbitrate:
+
+    if args.samecbr or args.vcbr:
+        v_cbr = get_cbr(args.input_file,'Video')
+        v_opts = ['-b:v', v_cbr]
+    elif args.vbitrate:
         v_opts = ['-b:v', args.vbitrate]
      
     v_args.extend(v_opts)
@@ -36,7 +44,11 @@ def get_aargs():
         a_args = ['-c:a', args.acodec]
  
     a_opts = ['-q:a', args.aquality]    
-    if args.abitrate:
+
+    if args.samecbr or args.acbr:
+        a_cbr = get_cbr(args.input_file,'Audio')
+        a_opts = ['-b:a', a_cbr]
+    elif args.abitrate:
         a_opts = ['-b:a', args.abitrate]
     
     a_args.extend(a_opts)
@@ -101,6 +113,81 @@ def construct_cmd():
     print main_args
     return main_args
     
+def get_video_codec(track):
+    if track.codec_info is None:
+        return "{}".format(track.codec)
+    return "{} ({})".format(track.codec, track.codec_info)
+    
+def get_video_bitrate(track):
+    if track.codec == "AVC":
+        if not track.other_maximum_bit_rate is None and track.other_nominal_bit_rate is None:
+            br = "{} / {} (nominal) / {} (max)".format(track.other_bit_rate[0], track.other_nominal_bit_rate[0], track.other_maximum_bit_rate[0])
+            return br
+    return "{}".format(track.other_bit_rate[0])
+
+def get_video_size(track):
+    if track.codec == "AVC":
+        if not track.other_source_stream_size is None:
+            return "{}".format(track.other_source_stream_size[0])
+    return "{}".format(track.other_stream_size[0])
+
+def get_audio_codec(track):
+    if track.codec_info is None:
+        return "{} ({})".format(track.codec, track.commercial_name)
+    return "{} ({} / {})".format(track.codec, track.codec_info, track.commercial_name)
+
+def get_audio_resolution(track):
+    if track.other_resolution is None:
+        track.other_resolution = ["?? bits"]
+    return "{}".format(track.other_resolution[0])   
+
+def get_audio_mode(track):
+    if track.bit_rate_mode is None:
+        track.bit_rate_mode = "Unspecified mode"
+    return "{}".format(track.bit_rate_mode)   
+
+def get_cbr(video_file, t_type):
+    media_info = MediaInfo.parse(video_file)
+    cbr = 0
+    
+    for track in media_info.tracks:
+        if track.track_type == t_type:
+            cbr=track.bit_rate
+    
+    return str(cbr)
+
+
+def report_stats(video_file):
+    media_info = MediaInfo.parse(video_file)
+
+    filestr = "{:<20} : {} ".format("File", video_file)
+    print "_" * len(filestr)
+    print filestr
+
+    for track in media_info.tracks:
+        if track.track_type == 'General':
+            print "{:<20} : {} ; Streams ({}V / {}A)".format("Codec", track.codec, track.count_of_video_streams, track.count_of_audio_streams)
+            print "{:<20} : {} ; {} ".format("Duration, Size", track.other_duration[0], track.other_file_size[0])
+            print
+        elif track.track_type == 'Video' :
+            print "{:<20} : {} (ID - {}); {}".format("Track", track.track_type, track.track_id, track.format)
+            print "{:<20} : {}".format("Codec", track.codec, get_video_codec(track))
+            print "{:<20} : {}".format("Duration", track.other_duration[0])
+            print "{:<20} : {}".format("Bit Rate", get_video_bitrate(track))
+            print "{:<20} : {}".format("Stream Size: ", get_video_size(track))
+            print
+        elif track.track_type == 'Audio' :
+            print "{:<20} : {} (ID - {}); {}".format("Track", track.track_type, track.track_id, track.format)
+            print "{:<20} : {}".format("Codec", get_audio_codec(track))
+            print "{:<20} : {}".format("Duration", track.other_duration[0])
+            print "{:<20} : {} ({})".format("Bit Rate", track.other_bit_rate[0], get_audio_mode(track))
+            print "{:<20} : {} Hz ({})".format("Sampling rate", track.sampling_rate, get_audio_resolution(track))
+            print "{:<20} : {}".format("Stream Size: ", track.other_stream_size[0])
+        else:
+            print "Omitting info for Track - {}".format(track.track_type)
+        
+    print "_" * len(filestr)
+   
 parser = argparse.ArgumentParser(description='Convert a file using ffmpeg')
 parser.add_argument ('input_file', help="File to be converted")
 #parser.add_argument ('-t', '--title', help="Title for filename. (Files are renamed to <Title> 01, <Title> 02, ...)", required=True)
@@ -111,40 +198,55 @@ cgroup = parser.add_mutually_exclusive_group()
 parser.add_argument ('-c:v', '--vcodec',    help="Video codec", default="libx264")
 vgroup.add_argument ('-crf',                help="Specify CRF for video usually between 18-24 (lower is better but bigger)", default='20')
 vgroup.add_argument ('-b:v', '--vbitrate',  help="Bitrate for video, overrides CRF if specified")
-parser.add_argument ('-c:a', '--acodec',    help="Audio codec", default="aac")
-agroup.add_argument ('-q:a', '--aquality',  help="Audio quality", default='100')
+#parser.add_argument ('-c:a', '--acodec',    help="Audio codec", default="aac")
+parser.add_argument ('-c:a', '--acodec',    help="Audio codec", default="libfdk_aac")
+agroup.add_argument ('-q:a', '--aquality',  help="Audio quality", default='5')
 agroup.add_argument ('-b:a', '--abitrate',  help="Audio bitrate. Overrides -aq")
 cgroup.add_argument ('-copy',               help="Copy both audio and video streams. Overrides others", action="store_true")
 cgroup.add_argument ('-copy:v', '--vcopy',  help="Copy only video stream", action="store_true")
 cgroup.add_argument ('-copy:a', '--acopy',  help="Copy only audioo stream", action="store_true")
 parser.add_argument ('--ffmpeg',            help="Path to ffmpeg.exe", default="ffmpeg.exe")
-parser.add_argument ('-p ',   '--preset',   help="Preset speed/qual tradeoff. slow(better) ... fast(worse)", default="slow")
-parser.add_argument ('-o',   '--output',    help="Specify output filename")
-parser.add_argument ('-n',   '--no_overwrite', help="Overwrite output file", action="store_true")
+parser.add_argument ('-p ',     '--preset', help="Preset speed/qual tradeoff. slow(better) ... fast(worse)", default="slow")
+parser.add_argument ('-o',      '--output', help="Specify output filename")
+parser.add_argument ('-n','--no_overwrite', help="Overwrite output file", action="store_true")
+parser.add_argument ('-i',      '--info',   help="Display media information", action="store_true")
+parser.add_argument ('-s',  '--samecbr',    help="Convert using same CBR as input (audio and video)", action="store_true")
+vgroup.add_argument ('-s:v',    '--vcbr',   help="Convert using same CBR as input (video)", action="store_true")
+agroup.add_argument ('-s:a',    '--acbr',   help="Convert using same CBR as input (audio)", action="store_true")
 parser.add_argument ('-other',              help="Other arguments to ffmpeg")
+
+
 args = parser.parse_args()
 
 if os.path.exists(args.input_file):
-    print "exists " + args.input_file
-    cmd_args = construct_cmd()
-
-    print "\n" + "-" * 22 + " STARTING " + "-" * 22
-    print " ".join([str(i) for i in cmd_args])    
-    print "\n" + "-" * 54
-    
-    ret = subprocess.call(cmd_args)
-    if not ret:
-        print "!!! SUCCESS !!!"
+    if args.info:
+        report_stats(args.input_file)
     else:
-        print "FFMpeg exited with error"
-
-    print "\n" + "-" * 22 + " FINISHED " + "-" * 22
-    print " ".join([str(i) for i in cmd_args])    
-    print "\n" + "-" * 54
-
-
-
-
+        time_start = strftime("%Y-%m-%d %H:%M:%S", localtime())        
+        cmd_args = construct_cmd()
+    
+        print "\n" + "-" * 22 + " STARTING " + "-" * 22
+        print " ".join([str(i) for i in cmd_args])    
+        print "\n" + "-" * 54
+        
+        t0 = time.time()        
+        ret = subprocess.call(cmd_args)
+        t1 = time.time()        
+        if not ret:
+            print "!!! SUCCESS !!!"
+            report_stats(args.input_file)
+            ofile=cmd_args[-1]
+            report_stats(ofile)
+        else:
+            print "FFMpeg exited with error"
+    
+        time_end = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        print "\n" + chr(205) * 22 + " REPORT " + chr(205) * 22 # print ════════════
+        print "{:<16}: {}".format("Command", " ".join([str(i) for i in cmd_args]))
+        print "{:<16}: {}".format("Start time", time_start)
+        print '{:<16}: {}'.format("End time", time_end)
+        print "{:<16}: {}".format("Conversion Time", strftime ("%H:%M:%S", time.gmtime(t1-t0)))
+        print "\n" + chr(196) * 52 # print ────────────────
 else:
     print "ERROR: No such file - ", args.input_file
     sys.exit(-1);
